c5abe3aec697e0183245f20ca2682383a316b91a
zhiyedan <shijia0620@126.com>
2017-04-18 15:31:01 +0800

    add minMunberInRotateArray

--- src/mianjing/question/MinNumberInRotateArray.java
+++ src/mianjing/question/MinNumberInRotateArray.java
@@ -1,5 +1,16 @@
 package mianjing.question;
+/*把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+        输入一个非递减排序的数组的一个旋转，输出旋转数组的最小元素。
+        例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1
 
+//TODO 输入String  转化为int[]
+Scanner sc = new Scanner(System.in);
+        String[] strings = sc.nextLine().split(" ");
+        int[] nums = new int[strings.length];
+        for(int i=0;i<strings.length;i++){
+            nums[i] = Integer.parseInt(strings[i]);
+        }
+*/
 /**
  * Created by zhiyedan on 4/18/17.
  */
