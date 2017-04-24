ee8927961d9fac4187fe13326e6d5bb54a9ef48e
zhiyedan <shijia0620@126.com>
2017-04-24 15:56:41 +0800

    Merge two List 递归版

--- src/SwordToOffer/question/MergeListNode.java
+++ src/SwordToOffer/question/MergeListNode.java
@@ -9,7 +9,7 @@ import java.util.List;
  * Created by zhiyedan on 4/24/17.
  */
 public class MergeListNode {
-    public ListNode solution(ListNode list1, ListNode list2) {
+    public ListNode solution1(ListNode list1, ListNode list2) {
         ListNode head = new ListNode(-1);
         ListNode result = head;
         while (list1 != null || list2 != null) {
@@ -25,5 +25,21 @@ public class MergeListNode {
             }
         }
         return result.next;
+    }
+    //递归版本
+    public ListNode solution(ListNode list1, ListNode list2) {
+        if(list1 == null)
+            return list2;
+        if(list2 == null)
+            return list1;
+        ListNode result = null;
+        if(list1.val<list2.val){
+            result = list1;
+            result.next = solution(list1.next,list2);
+        }else{
+            result = list2;
+            result.next = solution(list1,list2.next);
+        }
+        return result;
     }
 }
