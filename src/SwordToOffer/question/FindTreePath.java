c05c9e4c06637192e304b726207e0fbb17f8ef3a
zhiyedan <shijia0620@126.com>
2017-04-27 12:02:33 +0800

    查找路径和为特定值的所有路径（未完成）

--- src/SwordToOffer/question/FindTreePath.java
+++ src/SwordToOffer/question/FindTreePath.java
@@ -3,6 +3,9 @@ package SwordToOffer.question;
 import SwordToOffer.resc.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
 /**
  * 输入一颗二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所有路径
@@ -10,9 +13,25 @@ import java.util.ArrayList;
  * Created by zhiyedan on 4/26/17.
  */
 public class FindTreePath {
+    public int sum = 0;
+    public Stack<TreeNode> stack = new Stack<>();
+
     public ArrayList<ArrayList<Integer>> solution(TreeNode root, int target) {
+        if (root == null)
+            return null;
         ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-        ArrayList<Integer> arrayList = new ArrayList<>();
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        stack.add(root);
+        recursive(root, target, result);
+        return result;
+    }
+
+    public void recursive(TreeNode root, int target, ArrayList<ArrayList<Integer>> result) {
+        stack.add(root);
+        sum += root.val;
+        if (root.left == null && root.right == null)
+
+            if (sum == target)
 
     }
 }
