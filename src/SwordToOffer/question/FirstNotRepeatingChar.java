5c70c1df269cd8f3188d9bc5068a238eb4ea16f5
zhiyedan <shijia0620@126.com>
2017-05-17 17:41:34 +0800

    丑树！很巧妙的指针方法

---
+++ src/SwordToOffer/question/FirstNotRepeatingChar.java
@@ -0,0 +1,16 @@
+package SwordToOffer.question;
+
+import java.util.HashMap;
+
+/**
+ * 在一个字符串(1<=字符串长度<=10000，全部由大写字母组成)中找到第一个只出现一次的字符,并返回它的位置
+ * Created by zhiyedan on 5/17/17.
+ */
+public class FirstNotRepeatingChar {
+    public int solution(String str) {
+        int index = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        char [] chars =
+        return index;
+    }
+}
