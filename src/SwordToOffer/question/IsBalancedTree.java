910a62352a52c26601c2e19790d0bca959b53313
zhiyedan <shijia0620@126.com>
2017-06-19 08:45:10 +0800

    后续遍历判断是否为平衡二叉树

--- src/SwordToOffer/question/IsBalancedTree.java
+++ src/SwordToOffer/question/IsBalancedTree.java
@@ -1,6 +1,5 @@
 package SwordToOffer.question;
 
-<<<<<<< HEAD
 import SwordToOffer.resc.TreeNode;
 
 /**
@@ -11,39 +10,16 @@ import SwordToOffer.resc.TreeNode;
  */
 public class IsBalancedTree {
     public boolean solution(TreeNode root) {
-        if (root==null)
+        if (root == null)
             return true;
-        if(Math.abs(depthTree(root.right)-depthTree(root.left))>1)
+        if (Math.abs(depthTree(root.right) - depthTree(root.left)) > 1)
             return false;
-        return solution(root.left)&& solution(root.right);
+        return solution(root.left) && solution(root.right);
     }
-    public int depthTree(TreeNode root){
-        if (root==null)
-            return 0;
-        return Math.max(depthTree(root.left),depthTree(root.right))+1;
-=======
 
-import SwordToOffer.resc.TreeNode;
-
-/**
- * 判断是否为平衡二叉树。空：true， 左右子树均为平衡二叉树。左右子树的深度差不能大于1.
- * 采用后序遍历。
- * Created by zhiyedan on 17-6-14.
- */
-public class IsBalancedTree {
-    public boolean solution(TreeNode root){
-        if(root == null)
-            return true;
-        if(!solution(root.left) || !solution(root.right))
-            return false;
-        if(Math.abs(depth(root.left)-depth(root.right))>1)
-            return false;
-        return true;
-    }
-    public int depth(TreeNode root){
-        if(root==null)
+    public int depthTree(TreeNode root) {
+        if (root == null)
             return 0;
-        return Math.max(depth(root.left),depth(root.right))+1;
->>>>>>> origin/master
+        return Math.max(depthTree(root.left), depthTree(root.right)) + 1;
     }
 }
