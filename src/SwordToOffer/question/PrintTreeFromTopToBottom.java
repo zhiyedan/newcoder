7c29c0b1ce7af9225f7a15419970ea18477b4b23
zhiyedan <shijia0620@126.com>
2017-06-27 17:27:31 +0800

    从上到下打印二叉树（recommend*****）

--- src/SwordToOffer/question/PrintTreeFromTopToBottom.java
+++ src/SwordToOffer/question/PrintTreeFromTopToBottom.java
@@ -7,9 +7,32 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
+ * 返回结果不同，采用的方法不同，但整体思路相同，均为存队列，取队列。
  * Created by zhiyedan on 4/25/17.
  */
 public class PrintTreeFromTopToBottom {
+    ArrayList<ArrayList<Integer>> solution2(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        LinkedList<TreeNode> nodeList = new LinkedList<>();
+        if(pRoot==null)
+            return result;
+        nodeList.add(pRoot);
+        while (nodeList.size()!=0){
+            ArrayList<Integer> tempList = new ArrayList<>();
+            int size = nodeList.size();
+            //TODO : tip,通过开始的size来控制循环次数！
+            for(int i=0;i<size;i++){
+                TreeNode node = nodeList.poll();
+                tempList.add(node.val);
+                if(node.left!=null)
+                    nodeList.add(node.left);
+                if(node.right!=null)
+                    nodeList.add(node.right);
+            }
+            result.add(tempList);
+        }
+        return result;
+    }
     public static ArrayList<Integer> solution(TreeNode root) {
         ArrayList<Integer> result = new ArrayList<>();
         if (root == null)
