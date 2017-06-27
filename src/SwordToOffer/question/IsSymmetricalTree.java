451721a54630b8f067d4bb529f9b8aa0e7bbd2ab
zhiyedan <shijia0620@126.com>
2017-06-27 15:48:15 +0800

    to judge a tree wether is a symmetrical tree (左子树的右孩子与右子树的做孩子相等)。（recommend***）

---
+++ src/SwordToOffer/question/IsSymmetricalTree.java
@@ -0,0 +1,23 @@
+package SwordToOffer.question;
+
+import SwordToOffer.resc.TreeNode;
+
+/**
+ * 判断一颗二叉树是不是对称的
+ *
+ * Created by zhiyedan on 6/27/17.
+ */
+public class IsSymmetricalTree {
+    public boolean solution(TreeNode pRoot){
+        if(pRoot==null)
+            return true;
+        return compare(pRoot.left,pRoot.right);
+    }
+    public boolean compare(TreeNode leftRoot,TreeNode rightRoot){
+        if(leftRoot==null && rightRoot==null)
+            return true;
+        if(leftRoot==null || rightRoot==null || leftRoot.val!=rightRoot.val)
+            return false;
+        return compare(leftRoot.left,rightRoot.right) && compare(leftRoot.right,rightRoot.left);
+    }
+}
